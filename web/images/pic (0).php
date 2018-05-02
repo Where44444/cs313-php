88045d33ff827a694d741a9d2e015c914829e1ac
Alex Charles <alexjob44@gmail.com>
2018-05-02 17:13:55 -0600

    Homepage start5

--- web/images/pic (0).php
+++ web/images/pic (0).php
@@ -1,4 +1,7 @@
 <?php
+    echo '<h1><font size="100">Cool Stuff</font></h1>';
+    echo '<img src="greensquares.png" class=absolutepic alt="You did not see this!" style="float:right,width:75px;height:75px;border:0;">';
+    echo '<p>!</p><p>!</p><p>!</p><p>!</p>';
     echo '<link rel="stylesheet" type="text/css" href="../homestyles.css">';
     echo '<p align=center style="font-family:Calibri">501st Legion Stormtroopers Patrolling Earth</p>';
     echo '<img class="center" src="C3501st.jpg" alt="501st" style="border:0;">';
