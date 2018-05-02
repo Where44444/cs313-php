3cc3970c24c024f16affd2b5f49abd8918153035
Alex Charles <alexjob44@gmail.com>
2018-05-02 17:06:16 -0600

    Homepage start3

--- web/images/pic (0).php
+++ web/images/pic (0).php
@@ -1,5 +1,5 @@
 <?php
-    echo "<p align=center>501st Legion Stormtroopers Patrolling Earth</p>";
-    echo "<img align=center src="C3501st.jpg" alt="501st" style="border:0;">"
+    echo '<p align=center style="font-family:Calibri">501st Legion Stormtroopers Patrolling Earth</p>';
+    echo '<img align=center src="C3501st.jpg" alt="501st" style="border:0;">';
     //<p align=center><font face="Calibri" size = 50px>501st Legion Stormtroopers Patrolling Earth</font></p>
 ?>
