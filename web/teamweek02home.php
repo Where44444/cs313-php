090ee4720ecce4a0ef4c0e4b20976b47334e7547
Alex Charles <alexjob44@gmail.com>
2018-05-03 16:19:03 -0600

    Team Week 2 Activity

---
+++ web/teamweek02home.php
@@ -0,0 +1,13 @@
+<?php
+    echo '<style>
+    .message{
+      margin: 300px;
+      border: 100px solid rgb(100,200,100);
+      background-color: rgb(100,200,100);
+      font-size: 50px;
+      font-family: "Calibri", Times, serif;
+      color: white;
+    }
+    </style>'
+    echo "<p class=message>Welcome! You Are Not Signed In Boi!</p>";
+?>
