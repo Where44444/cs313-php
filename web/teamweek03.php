00012a0b59f07b2332d75a18d5e62f3e9f34bf1f
Alex Charles <alexjob44@gmail.com>
2018-05-08 16:28:36 -0600

    Message

--- web/teamweek03.php
+++ web/teamweek03.php
@@ -2,8 +2,10 @@
 $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
 $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
 
+if(isset($_POST['submit'])){
   if(isset($_POST['major'])){
     echo $_POST['major'];
   }
+}
 echo $name . "<br>" $email . "<br>";
 ?>
