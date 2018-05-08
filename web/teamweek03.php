451f8acf93720316cf5a19479915a1d4a779b846
Alex Charles <alexjob44@gmail.com>
2018-05-08 16:19:24 -0600

    Message

--- web/teamweek03.php
+++ web/teamweek03.php
@@ -1,7 +1,7 @@
 <?php
 $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
 $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
-$major = $_POST['major'];
+
 echo $name;
 echo <br>;
 echo $email;
