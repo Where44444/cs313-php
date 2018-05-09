e11a30dc668a2842f56f6d406b2eb82094d885a2
Alex Charles <alexjob44@gmail.com>
2018-05-09 16:00:26 -0600

    Message

---
+++ web/week03/purchase.php
@@ -0,0 +1,53 @@
+<?php
+// Start the session
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+   <head>
+   <script type="text/javascript" src="MyJavaScript.js"></script>
+       <title>Business Page</title>
+       <style>
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 125%;
+}
+
+td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+}
+
+tr:nth-child(even) {
+    background-color: #dddddd;
+}
+tr:nth-child(odd) {
+    background-color: white;
+}
+</style>
+   </head>
+   <link rel="stylesheet" type="text/css" href="styles.css">
+   <body>
+      <h1 class=Title2>PURCHASE</h1>
+      <a class=Link href="index.html">Home</a>
+      <a class=Link href="cart.html">Cart</a>
+      <a class=Link href="contact.html">Contact</a>
+      <p class=Info2>Purchase RC Brackets Here! Click to add 1 to cart!<br>
+      We may even send you some!</p>
+      <p id="Load1" style="color:blue"></p>
+
+      <?php
+      // Set session variables
+      $_SESSION["favcolor"] = "green";
+      $_SESSION["favanimal"] = "cat";
+      echo "Session variables are set.";
+      ?>
+
+      <p class=BottomBar2 style="position: absolute; top: 1410px; left: 50px">
+      Where44444 Copyright 2017 Alrights Reserved =D <br>
+      We are not responsible for any mysterious disappearances of money <br>
+      from your account.</p>
+   </body>
+</html>
