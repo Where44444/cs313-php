20b3d3b4f4cd5dc7e797a6163f957843ef47e875
Alex Charles <alexjob44@gmail.com>
2018-05-09 17:27:10 -0600

    Message

--- web/week03/MyJavaScript.js
+++ web/week03/MyJavaScript.js
@@ -1,5 +1,6 @@
 function addToCart(str)
 {
+  window.alert("Test");
       var xmlhttp = new XMLHttpRequest();
       xmlhttp.onreadystatechange = function() {
           if (this.readyState == 4 && this.status == 200) {
