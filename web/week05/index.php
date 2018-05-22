ec94bcc5fe840b52df8aafff3f27cbcf495be64c
Alex Charles <alexjob44@gmail.com>
2018-05-22 16:04:34 -0600

    Message

--- web/week05/index.php
+++ web/week05/index.php
@@ -18,12 +18,17 @@
       $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
       $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      foreach ($db->query('SELECT username, password FROM note_user') as $row)
+      echo '<form>
+      Book:<br>
+      <input type="text" name="firstname"><br>
+      </form>'
+      /*foreach ($db->query('SELECT book, verse FROM scriptures') as $row)
       {
-        echo 'user: ' . $row['username'];
-        echo ' password: ' . $row['password'];
+        echo 'user: ' . $row['book'];
+        echo ' password: ' . $row['verse'];
         echo '<br/>';
       }
+*/
 
        ?>
    </body>
