8ab00ca7fd54f5b82a6231c82cb1c87e412d15e3
Alex Charles <alexjob44@gmail.com>
2018-05-23 13:23:46 -0600

    Message

--- web/week05/index.php
+++ web/week05/index.php
@@ -4,18 +4,22 @@
        <title>Search Page</title>
    </head>
    <body>
-   <h1>Search for Database Entries boi!</h1>
+   <h1>Search for Database Entries Boi!</h1>
    <form>
    Search Users:<br>
-   <input type="text" name="user"><br>
+   <input type="text" name="user" placeholder="Definitely not a user"><br>
    </form>
    <form>
    Search Posts:<br>
-   <input type="text" name="post"><br>
+   <input type="text" name="post" placeholder="Cringy, Dank, and Otherwise Ludicrous Posts"><br>
    </form>
    <form>
    Search Words:<br>
-   <input type="text" name="word"><br>
+   <input type="text" name="word" placeholder="Pen is mightier than the sword, but not F-22 Miniguns"><br>
+   </form>
+   <form>
+   Search My Wallet:<br>
+   <input type="text" name="word" placeholder="No Money Here"><br>
    </form>
    </body>
 </html>
