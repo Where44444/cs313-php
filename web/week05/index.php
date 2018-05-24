c6ea236587949f5c74521cc9769a054296009f5f
Alex Charles <alexjob44@gmail.com>
2018-05-23 18:51:16 -0600

    Message

--- web/week05/index.php
+++ web/week05/index.php
@@ -16,5 +16,55 @@
    <input type="text" name="wallet" placeholder="No Money Here"><br>
    <input type="submit" name="WOAH"><br>
    </form>
+   <?php
+   $dbUrl = getenv('DATABASE_URL');
+
+   $dbopts = parse_url($dbUrl);
+
+   $dbHost = $dbopts["host"];
+   $dbPort = $dbopts["port"];
+   $dbUser = $dbopts["user"];
+   $dbPassword = $dbopts["pass"];
+   $dbName = ltrim($dbopts["path"],'/');
+
+   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+   $stmt = $db->prepare('SELECT * FROM userp');
+   $stmt2 = $db->prepare('SELECT * FROM post');
+   $stmt3 = $db->prepare('SELECT * FROM word');
+   $stmt->execute();
+   $stmt2->execute();
+   $stmt3->execute();
+   $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+   $rows2 = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+   $rows3 = $stmt3->fetchAll(PDO::FETCH_ASSOC);
+
+   echo "Users in the database:<br>";
+   foreach ($rows as $row)
+   {
+     echo "<b>".$row['username']." ".$row['password'] ."</b>";
+     echo '<br/>';
+   }
+   echo "<br>";
+
+   echo "Posts in dem databases:<br>";
+   foreach ($rows2 as $row)
+   {
+     echo "<b>".$row['post_text']." ".$row['cringy_count']."</b>";
+     echo '<br/>';
+   }
+   echo "<br>";
+
+   echo "Words in them database right there:<br>";
+   foreach ($rows3 as $row)
+   {
+     echo "<b>".$row['word']."</b>";
+     echo '<br/>';
+   }
+   echo "<br> No Wallet Database Needed";
+
+    ?>
    </body>
 </html>
