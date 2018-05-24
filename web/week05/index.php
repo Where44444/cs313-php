525c09399cf13a82ad85b222aab1a962903be255
Alex Charles <alexjob44@gmail.com>
2018-05-23 18:31:10 -0600

    Message

--- web/week05/index.php
+++ web/week05/index.php
@@ -8,16 +8,10 @@
    <form action="result.php" method="post">
    Search Users:<br>
    <input type="text" name="user" placeholder="Definitely not a user"><br>
-   </form>
-   <form action="result.php" method="post">
    Search Posts:<br>
    <input type="text" name="post" placeholder="Cringy or Dank Posts"><br>
-   </form>
-   <form action="result.php" method="post">
    Search Words:<br>
    <input type="text" name="word" placeholder="Pen is mightier than the sword, but not F-22 Miniguns"><br>
-   </form>
-   <form action="result.php" method="post">
    Search My Wallet:<br>
    <input type="text" name="wallet" placeholder="No Money Here"><br>
    </form>
