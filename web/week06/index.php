e5b903535d895467770ddb2c256bc7c99440e1e5
Alex Charles <alexjob44@gmail.com>
2018-05-31 17:55:44 -0600

    Message

--- web/week06/index.php
+++ web/week06/index.php
@@ -79,8 +79,10 @@
       $stmt->bindValue(':del', $del, PDO::PARAM_STR);
       $stmt->execute();
       $stmt = $db->prepare($sql5);
+      $stmt->bindValue(':del', $del, PDO::PARAM_STR);
       $stmt->execute();
       $stmt = $db->prepare($sql6);
+      $stmt->bindValue(':del', $del, PDO::PARAM_STR);
       $stmt->execute();
     }
  }
