6fd0e8166ab240ff743610b6b1f9aa90c0186805
Alex Charles <alexjob44@gmail.com>
2018-06-06 16:33:15 -0600

    Message

--- web/week07/index.php
+++ web/week07/index.php
@@ -11,6 +11,62 @@
 require("dbConnect.php");
 $db = get_db();
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $username = $_POST['user'];
+  $password = $_POST['password'];
+
+  $userid = $_POST['userid'];
+  $postp = $_POST['postp'];
+  $cringycount = $_POST['cringycount'];
+
+  $postid = $_POST['postid'];
+  $word = $_POST['word'];
+
+  $del = $_POST['del'];
+
+ $sql1 =  'INSERT INTO userp (username,password) VALUES (:username, :password)';
+ $sql2 =  'INSERT INTO post (user_id,post_text,cringy_count) VALUES (:userid, :postp, :cringycount)';
+ $sql3 =  'INSERT INTO word (post_id,word) VALUES (:postid, :word)';
+ $sql4 =  'DELETE FROM userp WHERE username = :del';
+ $sql5 =  'DELETE FROM post WHERE post_text = :del';
+ $sql6 =  'DELETE FROM word WHERE word = :del';
+
+   if ($username)
+   {
+     $stmt = $db->prepare($sql1);
+     $stmt->bindValue(':username', $username, PDO::PARAM_STR);
+     $stmt->bindValue(':password', $password, PDO::PARAM_STR);
+     $stmt->execute();
+   }
+   if ($userid)
+   {
+     $stmt = $db->prepare($sql2);
+     $stmt->bindValue(':userid', $userid, PDO::PARAM_INT);
+     $stmt->bindValue(':postp', $postp, PDO::PARAM_STR);
+     $stmt->bindValue(':cringycount', $cringycount, PDO::PARAM_INT);
+     $stmt->execute();
+   }
+   if ($postid)
+   {
+     $stmt = $db->prepare($sql3);
+     $stmt->bindValue(':postid', $postid, PDO::PARAM_INT);
+     $stmt->bindValue(':word', $word, PDO::PARAM_STR);
+     $stmt->execute();
+   }
+   if ($del)
+   {
+     $stmt = $db->prepare($sql6);
+     $stmt->bindValue(':del', $del, PDO::PARAM_STR);
+     $stmt->execute();
+     $stmt = $db->prepare($sql5);
+     $stmt->bindValue(':del', $del, PDO::PARAM_STR);
+     $stmt->execute();
+     $stmt = $db->prepare($sql4);
+     $stmt->bindValue(':del', $del, PDO::PARAM_STR);
+     $stmt->execute();
+   }
+}
+
 $stmt = $db->prepare('SELECT username, post_text, timestamp FROM post INNER JOIN userp ON post.user_id = userp.id ORDER BY timestamp DESC');
 $stmt->execute();
 $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -42,62 +98,6 @@ echo "<br>";
    // Connect to the database
    //require("dbConnect.php");
    //$db = get_db();
-
- if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-   $username = $_POST['user'];
-   $password = $_POST['password'];
-
-   $userid = $_POST['userid'];
-   $postp = $_POST['postp'];
-   $cringycount = $_POST['cringycount'];
-
-   $postid = $_POST['postid'];
-   $word = $_POST['word'];
-
-   $del = $_POST['del'];
-
-  $sql1 =  'INSERT INTO userp (username,password) VALUES (:username, :password)';
-  $sql2 =  'INSERT INTO post (user_id,post_text,cringy_count) VALUES (:userid, :postp, :cringycount)';
-  $sql3 =  'INSERT INTO word (post_id,word) VALUES (:postid, :word)';
-  $sql4 =  'DELETE FROM userp WHERE username = :del';
-  $sql5 =  'DELETE FROM post WHERE post_text = :del';
-  $sql6 =  'DELETE FROM word WHERE word = :del';
-
-    if ($username)
-    {
-      $stmt = $db->prepare($sql1);
-      $stmt->bindValue(':username', $username, PDO::PARAM_STR);
-      $stmt->bindValue(':password', $password, PDO::PARAM_STR);
-      $stmt->execute();
-    }
-    if ($userid)
-    {
-      $stmt = $db->prepare($sql2);
-      $stmt->bindValue(':userid', $userid, PDO::PARAM_INT);
-      $stmt->bindValue(':postp', $postp, PDO::PARAM_STR);
-      $stmt->bindValue(':cringycount', $cringycount, PDO::PARAM_INT);
-      $stmt->execute();
-    }
-    if ($postid)
-    {
-      $stmt = $db->prepare($sql3);
-      $stmt->bindValue(':postid', $postid, PDO::PARAM_INT);
-      $stmt->bindValue(':word', $word, PDO::PARAM_STR);
-      $stmt->execute();
-    }
-    if ($del)
-    {
-      $stmt = $db->prepare($sql6);
-      $stmt->bindValue(':del', $del, PDO::PARAM_STR);
-      $stmt->execute();
-      $stmt = $db->prepare($sql5);
-      $stmt->bindValue(':del', $del, PDO::PARAM_STR);
-      $stmt->execute();
-      $stmt = $db->prepare($sql4);
-      $stmt->bindValue(':del', $del, PDO::PARAM_STR);
-      $stmt->execute();
-    }
- }
 
    $stmt = $db->prepare('SELECT * FROM userp');
    $stmt2 = $db->prepare('SELECT * FROM post');
